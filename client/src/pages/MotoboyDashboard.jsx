edf752cc67d955b87a159cd34844b1b511e4e122
MarcioRosa-QuickQuote <appquickquote@gmail.com>
2026-05-30 01:34:35 -0300

    fix(motoboy): remove sino do topbar

--- client/src/pages/MotoboyDashboard.jsx
+++ client/src/pages/MotoboyDashboard.jsx
@@ -1414,19 +1414,6 @@ export default function MotoboyDashboard() {
               </div>
             )}
           </div>
-          {/* Sino */}
-          <div style={{ position: 'relative', cursor: 'pointer' }}>
-            <svg width="22" height="22" viewBox="0 0 24 24" fill="none"
-              stroke={mb.sub} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-              <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-            </svg>
-            {notifications.length > 0 && (
-              <div style={{ position: 'absolute', top: -4, right: -4, width: 16, height: 16, borderRadius: '50%', background: '#C62828', color: 'white', fontSize: 9, fontWeight: 700, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                {notifications.length > 9 ? '9+' : notifications.length}
-              </div>
-            )}
-          </div>
         </div>
       </div>
 
